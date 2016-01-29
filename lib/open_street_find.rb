0cdb4e4a1875121003af319766276bccd0bb6e54
YoanDieu <yoan.dieu@gmail.com>
2016-01-29 13:07:44 +0100

    made sure all arguments goes in a uri parser

--- lib/open_street_find.rb
+++ lib/open_street_find.rb
@@ -3,7 +3,7 @@ require 'net/http'
 module OpenStreetFind
   # Your code goes here...
   def self.find(query)
-    uri = URI('http://nominatim.openstreetmap.org/search/')
-    return Net::HTTP.get(uri + "q=" + query + "&" + "format=[json]")
+    uri = URI('http://nominatim.openstreetmap.org/search/'+ "q=" + query + "&" + "format=[json]")
+    return Net::HTTP.get(uri)
   end
 end
